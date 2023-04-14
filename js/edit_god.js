e1b96485b1adeccd035ea258a5ed940672f774c0
MatthewBrazill <matthew.brazill@datadoghq.com>
2023-04-14 13:29:52 +0100

    added "edit god" ability

---
+++ js/edit_god.js
@@ -0,0 +1,49 @@
+const url = new URL(window.location)
+$(document).ready(() => {
+    $.ajax({
+        url: "/gods/get",
+        method: "POST",
+        data: { godId: url.searchParams.get("godId") },
+        success: (res) => {
+            $("#edit-god-pantheon").val(res.god.pantheon),
+            $("#edit-god-name").val(res.god.name),
+            $("#edit-god-domain").val(res.god.domain)
+        },
+        error: (res) => {
+            $("#edit-god-form").attr("class", "ui error form")
+            $("#edit-god-error-message").text("Failed to get a god for this ID.")
+        }
+    })
+
+    $("#edit-god-form").submit((e) => {
+        e.preventDefault()
+        $.ajax({
+            url: "/gods/update",
+            method: "POST",
+            data: {
+                godId: url.searchParams.get("godId"),
+                pantheon: $("#edit-god-pantheon").val(),
+                name: $("#edit-god-name").val(),
+                domain: $("#edit-god-domain").val()
+            },
+            success: () => window.location = "/storage",
+            error: (res) => {
+                $("#edit-god-form").attr("class", "ui error form")
+                $("#edit-god-error-message").text(res.message)
+            }
+        })
+    })
+
+    $("delete-god-button").click(() => {
+        $.ajax({
+            url: "/gods/delete",
+            method: "POST",
+            data: { godId: url.searchParams.get("godId") },
+            success: () => window.location = "/storage",
+            error: (res) => {
+                $("#edit-god-form").attr("class", "ui error form")
+                $("#edit-god-error-message").text(res.message)
+            }
+        })
+    })
+})
