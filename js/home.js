0105547721d0cfd3aae657af3cd84898f50fff09
MatthewBrazill <matthew.brazill@datadoghq.com>
2023-04-14 09:29:11 +0100

    fixed placeholder js files

--- js/home.js
+++ js/home.js
@@ -1,17 +1,1 @@
-$(document).ready(() => {
-    $("#god-button").click(() => {
-        $.ajax({
-            url: "/login",
-            method: "POST",
-            data: {
-                username: $("#login-username").val(),
-                password: $("#login-password").val()
-            },
-            success: (res) => window.location = "/home",
-            error: (res) => {
-                $("#login-form").attr('class', 'ui error form')
-                $("#login-error-message").text(res.message)
-            }
-        })
-    })
-})
+$(document).ready(() => { })
