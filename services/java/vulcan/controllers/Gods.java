9e52ea7bcd84063ddc3227677cb7d0a94c79c0c3
MatthewBrazill <matthew.brazill@datadoghq.com>
2023-09-13 14:23:15 +0100

    added gods/update

--- services/java/vulcan/controllers/Gods.java
+++ services/java/vulcan/controllers/Gods.java
@@ -5,13 +5,16 @@ import java.util.HashMap;
 import java.util.Random;
 
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import com.mongodb.client.result.InsertOneResult;
+import com.mongodb.client.result.UpdateResult;
 
 import io.opentracing.Span;
 import io.opentracing.log.Fields;
@@ -114,7 +117,42 @@ public class Gods {
     @ResponseBody
     @RequestMapping(value = "/gods/update", method = RequestMethod.POST)
     public HashMap<String, Object> godUpdateAPI(HttpServletRequest req, HttpServletResponse res) {
-        return null;
+        String[][] params = { { "godId", "^[a-zA-Z0-9]{5}$" }, { "pantheon", "^[a-zA-Z]{1,32}$" }, { "name", "^[a-zA-Z]{1,32}$" }, { "domain", "^[0-9a-zA-Z ]{1,32}$" } };
+        HashMap<String, Object> reqBody = Helpers.decodeBody(req);
+        HashMap<String, Object> resBody = new HashMap<String, Object>();
+        Span span = GlobalTracer.get().activeSpan();
+
+        if (!Helpers.validate(req, params)) {
+            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resBody.put("message", "There was an issue with your request.");
+            return resBody;
+        }
+
+        try {
+            Bson update = Updates.combine(
+                    Updates.set("name", reqBody.get("name")),
+                    Updates.set("pantheon", reqBody.get("pantheon")),
+                    Updates.set("domain", reqBody.get("domain")));
+            UpdateResult result = Databases.godDatabse().updateOne(Filters.eq("godId", reqBody.get("godId")), update);
+
+            if (result.wasAcknowledged()) {
+                res.setStatus(HttpServletResponse.SC_OK);
+                resBody.put("message", "Successfully updated god.");
+                return resBody;
+            } else {
+                res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                resBody.put("message", "Couldn't find a god with that ID.");
+                return resBody;
+            }
+        } catch (Exception e) {
+			res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			resBody.put("message", "There was an issue with the Server, please try again later.");
+
+			span.setTag(Tags.ERROR, true);
+			span.log(Collections.singletonMap(Fields.ERROR_OBJECT, e));
+
+			return resBody;
+        }
     }
 
     @ResponseBody
