d3fa9d16e487ea63c0096f7690f82f223af517a1
MatthewBrazill <matthew.brazill@datadoghq.com>
2023-09-28 11:54:06 +0200

    added helpers tracing

--- services/node/helpers.js
+++ services/node/helpers.js
@@ -1,50 +1,76 @@
 'use strict'
 
 // Imports
+const tracer = require("dd-trace").tracer
 const logger = require("./logger.js")
 const pgdb = require("./postgres.js")
 
 const helpers = {
     async authorize(req) {
-        var username = req.session.username
+        return await tracer.trace("vulcan.helper", { resource: "authorize" }, async () => {
+            try {
+                const span = tracer.scope().active()
+                var username = req.session.username
 
-        if (username == null || username == "") {
-            logger.debug("authorize using api key")
-            var result = await pgdb.query("SELECT * FROM apikeys WHERE apikey = $1::text", [req.headers["api-key"]])
-        } else {
-            logger.debug("authorize using username")
-            var result = await pgdb.query("SELECT * FROM users WHERE username = $1::text", [username])
-        }
+                if (username == null || username == "") {
+                    logger.debug("authorize using api key")
+                    span.setTag("auth_method", "api_key")
+                    var result = await pgdb.query("SELECT * FROM apikeys WHERE apikey = $1::text", [req.headers["api-key"]])
+                } else {
+                    logger.debug("authorize using username")
+                    span.setTag("auth_method", "api_key")
+                    var result = await pgdb.query("SELECT * FROM users WHERE username = $1::text", [username])
+                }
 
-        logger.debug({ message: `authorize returned permissions: ${result.rows[0].permissions}`, permissions: result.rows[0].permissions })
-        if (result.rowCount > 0) return result.rows[0].permissions
-        else return "no_auth"
+                logger.debug({ message: `authorize returned permissions: ${result.rows[0].permissions}`, permissions: result.rows[0].permissions })
+                if (result.rowCount > 0) {
+                    span.setTag("auth", true)
+                    return result.rows[0].permissions
+                } else {
+                    span.setTag("auth", false)
+                    return "no_auth"
+                }
+            } catch (err) {
+                span.setTag("auth", false)
+                return "error"
+            }
+        })
     },
 
     async validate(params, tests) {
-        for (var test of tests) {
-            if (typeof params[test[0]] === "undefined") {
-                logger.debug({
-                    message: `Parameter unavailable for ${params[test[0]]}`,
-                    key: test[0],
-                    pattern: test[1],
-                    value: params[test[0]]
-                })
-                return false
-            }
+        return await tracer.trace("vulcan.helper", { resource: "validate" }, async () => {
+            try {
+                const span = tracer.scope().active()
+                for (var test of tests) {
+                    if (typeof params[test[0]] === "undefined") {
+                        logger.debug({
+                            message: `Parameter unavailable for ${params[test[0]]}`,
+                            key: test[0],
+                            pattern: test[1],
+                            value: params[test[0]]
+                        })
+                        span.setTag("valid", false)
+                        return false
+                    }
 
-            if (!params[test[0]].match(test[1])) {
-                logger.debug({
-                    message: `Validation failed for ${params[test[0]]}`,
-                    key: test[0],
-                    pattern: test[1],
-                    value: params[test[0]]
-                })
+                    if (!params[test[0]].match(test[1])) {
+                        logger.debug({
+                            message: `Validation failed for ${params[test[0]]}`,
+                            key: test[0],
+                            pattern: test[1],
+                            value: params[test[0]]
+                        })
+                        span.setTag("valid", false)
+                        return false
+                    }
+                }
+                logger.debug("validated request body")
+                span.setTag("valid", true)
+                return true
+            } catch (err) {
                 return false
             }
-        }
-        logger.debug("validated request body")
-        return true
+        })
     }
 }
 
