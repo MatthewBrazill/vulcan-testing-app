bf3fc83dd8ad9a1c4af5bd841c0d813a1970ba28
MatthewBrazill <matthew.brazill@datadoghq.com>
2023-10-09 10:31:49 +0200

    added logout button

--- js/global.js
+++ js/global.js
@@ -1,1 +1,11 @@
-$(document).ready(() => {})
+$(document).ready(() => {
+    $("#logout-button").click((e) => {
+        e.preventDefault()
+        $.ajax({
+            url: "/logout",
+            method: "GET",
+            success: () => window.location = "/login",
+            error: (res) => alert(`Logout failed: ${res.status} - ${res.message}`)
+        })
+    })
+})
