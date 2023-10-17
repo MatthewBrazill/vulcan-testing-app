143965a9d4ba1ebebad6e40374c6d63d766a0a9b
MatthewBrazill <matthew.brazill@datadoghq.com>
2023-10-17 12:12:21 +0200

    forced /vulcan removal

--- startup.sh
+++ startup.sh
@@ -4,8 +4,9 @@
 
 if [ "$DD_ENV" == "kubernetes" ]
 then
-    rm /vulcan -R
+    rm -rf /vulcan
     mkdir /vulcan
+    git init
     git clone -q https://github.com/MatthewBrazill/vulcan-testing-app.git
 fi
 
