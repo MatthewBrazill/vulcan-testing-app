4ccf5e8a2a833a014b3d747d5d43d11d3c71518c
MatthewBrazill <matthew.brazill@datadoghq.com>
2023-11-29 17:32:49 +0100

    added UI error button

--- services/frontend/javascript/error.js
+++ services/frontend/javascript/error.js
@@ -1,1 +1,10 @@
-$(document).ready(() => { })
+$(document).ready(() => {
+    $("#ui-error-button").click(() => {
+        $.ajax({
+            url: "https://random-word-api.herokuapp.com/word",
+            method: "GET",
+            success: (res) => { throw new `Example error: ${res.message}` },
+            error: (res) => alert(`Request failed: ${res.status} - ${res.message}`)
+        })
+    })
+})
