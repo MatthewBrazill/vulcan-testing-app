b39058263271b65a4c4582e90b9466f0de0df906
MatthewBrazill <matthew.brazill@datadoghq.com>
2023-11-29 13:20:57 +0100

    replaces string comparison with equals

--- services/java/vulcan/Databases.java
+++ services/java/vulcan/Databases.java
@@ -29,13 +29,13 @@ public class Databases {
             Class.forName("org.postgresql.Driver");
 
             String postgresURL;
-            if (System.getProperty("dd.env") == "kubernetes") {
+            if (System.getProperty("dd.env").equals("kubernetes")) {
                 postgresURL = "jdbc:postgresql://host.minikube.internal:5432/vulcan_users";
             } else {
                 postgresURL = "jdbc:postgresql://user-database:5432/vulcan_users";
             }
 
-            Connection conn = DriverManager.getConnection(postgresURL, "vulcan", "yKCstvg4hrB9pmDP@");
+            Connection conn = DriverManager.getConnection(postgresURL, "vulcan", "yKCstvg4hrB9pmDP");
             statement = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
         } catch (Exception e) {
             span.setTag(Tags.ERROR, true);
