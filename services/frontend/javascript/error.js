51e8cac2a49d3b97db1aeb5bd3ce447661f8af2e
MatthewBrazill <matthew.brazill@datadoghq.com>
2023-11-29 17:33:19 +0100

    fixed source map names and upload

--- services/frontend/javascript/error.js
+++ services/frontend/javascript/error.js
@@ -1,9 +1,9 @@
 $(document).ready(() => {
     $("#ui-error-button").click(() => {
         $.ajax({
-            url: "https://random-word-api.herokuapp.com/word",
+            url: "https://random-word-api.herokuapp.com/word?number=3",
             method: "GET",
-            success: (res) => { throw new `Example error: ${res.message}` },
+            success: (res) => { throw new Error(`Example error: ${res[0]} - ${res[1]} - ${res[2]}`) },
             error: (res) => alert(`Request failed: ${res.status} - ${res.message}`)
         })
     })
