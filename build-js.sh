27766c4e7828c3ce496bed293d1a8c2c9e66fdf2
MatthewBrazill <matthew.brazill@datadoghq.com>
2023-12-23 08:25:23 +0100

    fixed sourcemap script to include content

--- build-js.sh
+++ build-js.sh
@@ -2,5 +2,6 @@ for f in services/frontend/javascript/*.js; do
     java -jar ~/Playground/closure-compiler-v20231112.jar \
     --js $f \
     --js_output_file statics/js/$(basename $f .js).min.js \
-    --create_source_map statics/js/$(basename $f .js).min.js.map
+    --create_source_map statics/js/$(basename $f .js).min.js.map \
+    --source_map_include_content true
 done
