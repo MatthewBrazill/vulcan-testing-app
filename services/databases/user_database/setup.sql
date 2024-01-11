3cdf1206a8c3921698cf41d66c463a200b8266d0
MatthewBrazill <matthew.brazill@datadoghq.com>
2024-01-11 13:39:38 +0100

    added postgres config files

--- services/databases/user_database/setup.sql
+++ services/databases/user_database/setup.sql
@@ -1,5 +1,41 @@
 USE vulcan_users;
 
+CREATE USER datadog WITH password '5aae8c35f7e16245';
+ALTER ROLE datadog INHERIT;
+
+
+-- Set up the nececary schemas for Datadog
+CREATE SCHEMA datadog;
+GRANT USAGE ON SCHEMA datadog TO datadog;
+GRANT USAGE ON SCHEMA public TO datadog;
+GRANT pg_monitor TO datadog;
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+
+
+-- Create function to collect the explainplans
+CREATE OR REPLACE FUNCTION datadog.explain_statement(
+   l_query TEXT,
+   OUT explain JSON
+)
+RETURNS SETOF JSON AS
+$$
+DECLARE
+curs REFCURSOR;
+plan JSON;
+
+BEGIN
+   OPEN curs FOR EXECUTE pg_catalog.concat('EXPLAIN (FORMAT JSON) ', l_query);
+   FETCH curs INTO plan;
+   CLOSE curs;
+   RETURN QUERY SELECT plan;
+END;
+$$
+LANGUAGE 'plpgsql'
+RETURNS NULL ON NULL INPUT
+SECURITY DEFINER;
+
+
+-- Add the user and API key data to the database
 CREATE TABLE users (
     username varchar(32),
     password varchar(64),
@@ -10,8 +46,6 @@ CREATE TABLE apikeys (
     apikey char(32),
     permissions varchar(32)
 );
-
-
 
 INSERT INTO users VALUES ('matthew', 'testingpassword', 'admin');
 INSERT INTO users VALUES ('synthetics', '3J^eZ%u[D+', 'user');
