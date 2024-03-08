8634256f0b3ff9f23d6006c6cfd7df527c9e897b
MatthewBrazill <matthew.brazill@datadoghq.com>
2024-03-08 18:20:22 +0100

    added build scripts folder

--- build_scripts/build-js.sh
+++ build_scripts/build-js.sh
@@ -1,7 +1,7 @@
 for f in services/frontend/javascript/*.js; do
     java -jar ~/Playground/closure-compiler-v20231112.jar \
     --js $f \
-    --js_output_file statics/js/$(basename $f .js).min.js \
-    --create_source_map statics/js/$(basename $f .js).min.js.map \
+    --js_output_file services/frontend/statics/js/$(basename $f .js).min.js \
+    --create_source_map services/frontend/statics/js/$(basename $f .js).min.js.map \
     --source_map_include_content true
 done
