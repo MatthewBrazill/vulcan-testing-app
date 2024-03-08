a8a8419ad9b063580ee94c1593f9a0629abf7daf
MatthewBrazill <matthew.brazill@datadoghq.com>
2024-03-08 18:21:24 +0100

    fixed startup files

--- startup.sh
+++ startup.sh
@@ -14,13 +14,29 @@ export VULCAN_COMMIT_SHA=$(git rev-parse HEAD)
 if [ "$DD_SERVICE" == "vulcan-go" ]
 then
     go mod download && go mod verify
-    go build -o /usr/local/bin/vulcan -tags appsec ./services/golang/...
+    go build -o /usr/local/bin/vulcan -tags appsec ./vulcan/...
     vulcan
 elif [ "$DD_SERVICE" = "vulcan-js" ]
 then
     npm install .
-    node ./services/node/app.js
+    npm start
 elif [ "$DD_SERVICE" = "vulcan-java" ]
+then
+    wget -nc -nv -O /vulcan/dd-java-agent.jar https://dtdg.co/latest-java-tracer
+    mvn install
+    java -javaagent:/vulcan/dd-java-agent.jar \
+        -Dvulcan.session.key=2PbmuNW_uRkaf6Kux!ByK!yT!UmMZZ9B \
+        -Ddd.env=$DD_ENV \
+        -Ddd.service=$DD_SERVICE \
+        -Ddd.version=$DD_VERSION \
+        -Ddd.profiling.enabled=true \
+        -Ddd.appsec.enabled=true \
+        -Ddd.iast.enabled=true \
+        -Ddd.dbm.propagation.mode=full \
+        -Ddd.service.mapping=redis:session-store,postgresql:user-database,mongo:god-database \
+        -Ddd.tags=git.commit.sha:$(git rev-parse HEAD),git.repository_url:github.com/MatthewBrazill/vulcan-testing-app \
+        -jar ./target/vulcan.jar
+elif [ "$DD_SERVICE" = "vulcan-auth" ]
 then
     wget -nc -nv -O /usr/src/dd-java-agent.jar https://dtdg.co/latest-java-tracer
     mvn install
@@ -34,11 +50,8 @@ then
         -Ddd.iast.enabled=true \
         -Ddd.dbm.propagation.mode=full \
         -Ddd.service.mapping=redis:session-store,postgresql:user-database,mongo:god-database \
-        -Ddd.tags=git.commit.sha:$(git rev-parse HEAD),git.repository_url:github.com/MatthewBrazill/vulcan-testing-app -jar \
-        ./target/vulcan.jar
-elif [ "$DD_SERVICE" = "vulcan-py" ]
-then
-    echo "Not yet implemented"
+        -Ddd.tags=git.commit.sha:$(git rev-parse HEAD),git.repository_url:github.com/MatthewBrazill/vulcan-testing-app \
+        -jar ./target/vulcan.jar
 elif [ "$DD_SERVICE" = "vulcan-flutter" ]
 then
     echo "Not yet implemented"
