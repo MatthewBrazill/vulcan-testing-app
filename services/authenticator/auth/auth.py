0c9768b19be5ad24441c52a6b577a046f1fee021
MatthewBrazill <matthew.brazill@datadoghq.com>
2024-04-08 19:13:18 +0200

    moved database connection to helper function

--- services/authenticator/auth/auth.py
+++ services/authenticator/auth/auth.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 # Imports
-import asyncpg
 from helpers import validate
+from helpers import userDatabase
 from fastapi import Request, FastAPI
 from fastapi.responses import JSONResponse
 from ddtrace import tracer
@@ -15,17 +15,17 @@ app = FastAPI()
 async def auth(request: Request) -> JSONResponse:
 
     body = await request.json()
-    if validate(body, [["apiKey", "^[a-f0-9]{32}$"], ["username", "^[a-zA-Z]{1,32}$"], ["password", "^.{1,64}$"]]) == False:
+    if await validate(body, [["apiKey", "^[a-f0-9]{32}$"], ["username", "^[a-zA-Z]{1,32}$"], ["password", "^.{1,64}$"]]) == False:
         return JSONResponse(content={"perms": "no_auth"}, status_code=400)
 
-    db = await asyncpg.connect(host="user-database", port="5432", user="vulcan", password="yKCstvg4hrB9pmDP", database="vulcan_users")
+    database = await userDatabase()
     if "apiKey" in body.keys():
-        user = await db.fetch("SELECT * FROM apikeys WHERE apikey = $1", body["apiKey"])
+        user = await database.fetch("SELECT * FROM apikeys WHERE apikey = $1", body["apiKey"])
         if user != None and len(user) == 1:
             return JSONResponse(content={"perms": user[0].get("permissions")}, status_code=200)
 
     elif "username" in body.keys():
-        user = await db.fetch("SELECT * FROM users WHERE username = $1", body["username"])
+        user = await database.fetch("SELECT * FROM users WHERE username = $1", body["username"])
         if user != None and len(user) == 1 and user[0].get("password") == body["password"]:
             return JSONResponse(content={"perms": user[0].get("permissions")}, status_code=200)
 
--- services/authenticator/auth/helpers.py
+++ services/authenticator/auth/helpers.py
@@ -1,11 +1,25 @@
 # Imports
 import re
+import asyncpg
 from ddtrace import tracer
 
-@tracer.wrap(name="authenticator.helpers", resource="validate")
-def validate(params, tests):
+# Variables
+database = None
+
+@tracer.wrap(name="authenticator.helper", resource="validate")
+async def validate(params, tests):
     for test in tests:
         if test[0] in params.keys():
             if re.search(test[1], params[test[0]]) == None:
                 return False
     return True
+
+
+@tracer.wrap(name="authenticator.database", resource="userDatabase")
+async def userDatabase():
+    global database
+    if isinstance(database, asyncpg.Connection):
+        return database
+    else:
+        database = await asyncpg.connect(host="user-database", port="5432", user="vulcan", password="yKCstvg4hrB9pmDP", database="vulcan_users")
+        return database
