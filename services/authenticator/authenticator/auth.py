6092aff840c51c7336012bdfa9908476b0626d37
MatthewBrazill <matthew.brazill@datadoghq.com>
2024-04-10 14:44:49 +0200

    renamed auth folder to authenticator

--- services/authenticator/authenticator/auth.py
+++ services/authenticator/authenticator/auth.py
@@ -16,17 +16,17 @@ async def auth(request: Request) -> JSONResponse:
 
     body = await request.json()
     if await validate(body, [["apiKey", "^[a-f0-9]{32}$"], ["username", "^[a-zA-Z]{1,32}$"], ["password", "^.{1,64}$"]]) == False:
-        return JSONResponse(content={"perms": "no_auth"}, status_code=400)
+        return JSONResponse(content={"permissions": "none"}, status_code=400)
 
     database = await userDatabase()
     if "apiKey" in body.keys():
         user = await database.fetch("SELECT * FROM apikeys WHERE apikey = $1", body["apiKey"])
         if user != None and len(user) == 1:
-            return JSONResponse(content={"perms": user[0].get("permissions")}, status_code=200)
+            return JSONResponse(content={"permissions": user[0].get("permissions")}, status_code=200)
 
     elif "username" in body.keys():
         user = await database.fetch("SELECT * FROM users WHERE username = $1", body["username"])
         if user != None and len(user) == 1 and user[0].get("password") == body["password"]:
-            return JSONResponse(content={"perms": user[0].get("permissions")}, status_code=200)
+            return JSONResponse(content={"permissions": user[0].get("permissions")}, status_code=200)
 
-    return JSONResponse(content={"perms": "no_auth"}, status_code=200)
+    return JSONResponse(content={"permissions": "none"}, status_code=400)
