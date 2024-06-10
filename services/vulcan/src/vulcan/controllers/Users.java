3c42354726a39dd202fa309cb9c77195599f1c61
MatthewBrazill <matthew.brazill@datadoghq.com>
2024-06-10 14:49:49 +0200

    migrated login to authenticator service

--- services/vulcan/src/vulcan/controllers/Users.java
+++ services/vulcan/src/vulcan/controllers/Users.java
@@ -1,6 +1,7 @@
 package vulcan.controllers;
 
-import java.sql.ResultSet;
+import java.net.URI;
+import java.net.http.HttpResponse;
 import java.util.Collections;
 import java.util.HashMap;
 
@@ -18,15 +19,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import vulcan.Helpers;
-import vulcan.Databases;
 
 @Controller
 public class Users {
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String loginPage(Model model) {
+	public String loginPage(HttpServletRequest req, Model model) {
+		req.getSession().invalidate();
 		model.addAttribute("title", "Login Page");
-		model.addAttribute("language", "Java");
 		return "login";
 	}
 
@@ -40,41 +40,57 @@ public class Users {
 	@ResponseBody
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public HashMap<String, Object> loginAPI(HttpServletRequest req, HttpServletResponse res) {
-		String[][] params = { { "username", "^[a-zA-Z]{1,32}$" }, { "password", "^.{1,64}$" } };
-		HashMap<String, Object> body = new HashMap<String, Object>();
-		HashMap<String, Object> reqBody = Helpers.decodeBody(req);
+ 		// Function variables
 		Span span = GlobalTracer.get().activeSpan();
+		HashMap<String, Object> body = Helpers.decodeBody(req);
+		HashMap<String, Object> output = new HashMap<String, Object>();
 
 		try {
 			// Validate the user input
 			if (!Helpers.validate(body)) {
 				res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-				body.put("message", "There was an issue with your request.");
-				return body;
+				output.put("message", "There was an issue with your request.");
+				return output;
 			}
 
-			ResultSet result = Databases.userDatabase().executeQuery("SELECT * FROM users WHERE username = '" + reqBody.get("username") + "'");
-			result.first();
-			String password = result.getString("password");
+            // Generate the authorization request body
+            HashMap<String, Object> auth = new HashMap<String, Object>();
+            auth.put("username", body.get("username"));
+			//auth.put("pwHash", DigestUtils.sha256Hex(body.get("password").toString()));
+			auth.put("pwHash", body.get("password"));
 
-			if (reqBody.get("password").equals(password)) {
-				req.getSession().setAttribute("username", reqBody.get("username"));
-				res.setStatus(HttpServletResponse.SC_OK);
-				body.put("message", "Successfully logged in.");
-				return body;
-			} else {
-				res.setStatus(HttpServletResponse.SC_FORBIDDEN);
-				body.put("message", "Your login details are incorrect.");
-				return body;
+            // Make authentication request to authenticator service
+            HttpResponse<String> response = Helpers.httpPostRequest(new URI("https://authenticator:2884/authenticate"), auth);
+
+ 			// Handle response
+			switch (response.statusCode()) {
+   				case HttpServletResponse.SC_OK:
+					res.setStatus(HttpServletResponse.SC_OK);
+					req.getSession().setAttribute("username", body.get("username"));
+					output.put("message", "Successfully logged in.");
+					return output;
+
+				case HttpServletResponse.SC_UNAUTHORIZED:
+					res.setStatus(HttpServletResponse.SC_FORBIDDEN);
+					output.put("message", "Your login details are incorrect.");
+					return output;
+
+				case HttpServletResponse.SC_INTERNAL_SERVER_ERROR:
+					res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+					output.put("message", "There was an issue with the Server, please try again later.");
+					return output;
+
+				default:
+					throw new Exception("VulcanError: couldn't authorize request");
 			}
 		} catch (Exception e) {
 			res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			body.put("message", "There was an issue with the Server, please try again later.");
+			output.put("message", "There was an issue with the Server, please try again later.");
 
 			span.setTag(Tags.ERROR, true);
 			span.log(Collections.singletonMap(Fields.ERROR_OBJECT, e));
 
-			return body;
+			return output;
 		}
 	}
 
