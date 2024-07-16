9894307124067a1d1f571910a7043449a770dedc
MatthewBrazill <matthew.brazill@datadoghq.com>
2024-07-16 18:21:57 +0200

    split user and auth and added code for user create and delete

---
+++ services/vulcan/src/vulcan/controllers/Authenticate.java
@@ -0,0 +1,108 @@
+package vulcan.controllers;
+
+import java.net.URI;
+import java.net.http.HttpResponse;
+import java.util.Collections;
+import java.util.HashMap;
+
+import io.opentracing.Span;
+import io.opentracing.log.Fields;
+import io.opentracing.tag.Tags;
+import io.opentracing.util.GlobalTracer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import vulcan.Helpers;
+
+@Controller
+public class Authenticate {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginPage(HttpServletRequest req, Model model) {
+        // Function variables
+        Logger logger = LogManager.getLogger("vulcan");
+        HashMap<String, Object> body = Helpers.decodeBody(req);
+
+        // Log out user
+        req.getSession().invalidate();
+        logger.info(body.get("username") + " logged out");
+
+        model.addAttribute("title", "Login Page");
+        return "login";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public HashMap<String, Object> loginAPI(HttpServletRequest req, HttpServletResponse res) {
+        // Function variables
+        Span span = GlobalTracer.get().activeSpan();
+        HashMap<String, Object> body = Helpers.decodeBody(req);
+        HashMap<String, Object> output = new HashMap<String, Object>();
+        Logger logger = LogManager.getLogger("vulcan");
+
+        try {
+            // Validate the user input
+            if (!Helpers.validate(body)) {
+                res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                output.put("message", "There was an issue with your request.");
+                return output;
+            }
+
+            // Generate the authorization request body
+            HashMap<String, Object> auth = new HashMap<String, Object>();
+            auth.put("username", body.get("username"));
+            // auth.put("pwHash", DigestUtils.sha256Hex(body.get("password").toString()));
+            auth.put("pwHash", body.get("password"));
+
+            // Make authentication request to authenticator service
+            HttpResponse<String> response = Helpers.httpPostRequest(new URI("https://authenticator:2884/authenticate"), auth);
+
+            // Handle response
+            switch (response.statusCode()) {
+                case HttpServletResponse.SC_OK:
+                    res.setStatus(HttpServletResponse.SC_OK);
+                    req.getSession().setAttribute("username", body.get("username"));
+                    output.put("message", "Successfully logged in.");
+                    logger.info(body.get("username") + " logged in");
+                    return output;
+
+                case HttpServletResponse.SC_UNAUTHORIZED:
+                    res.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                    output.put("message", "Your login details are incorrect.");
+                    logger.info(body.get("username") + " failed to log in");
+                    return output;
+
+                default:
+                    throw new Exception("VulcanError: couldn't authorize request");
+            }
+        } catch (Exception e) {
+            res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            output.put("message", "There was an issue with the Server, please try again later.");
+
+            span.setTag(Tags.ERROR, true);
+            span.log(Collections.singletonMap(Fields.ERROR_OBJECT, e));
+
+            return output;
+        }
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public void logoutAPI(HttpServletRequest req, HttpServletResponse res) {
+        // Function variables
+        Logger logger = LogManager.getLogger("vulcan");
+        HashMap<String, Object> body = Helpers.decodeBody(req);
+
+        // Log out user
+        req.getSession().invalidate();
+        logger.info(body.get("username") + " logged out");
+        res.setStatus(HttpServletResponse.SC_OK);
+    }
+}
--- services/vulcan/src/vulcan/controllers/Users.java
+++ services/vulcan/src/vulcan/controllers/Users.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -25,90 +24,10 @@ import vulcan.Helpers;
 @Controller
 public class Users {
 
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String loginPage(HttpServletRequest req, Model model) {
-		// Function variables
-		Logger logger = LogManager.getLogger("vulcan");
-		HashMap<String, Object> body = Helpers.decodeBody(req);
-
-		// Log out user
-		req.getSession().invalidate();
-		logger.info(body.get("username") + " logged out");
-
-		model.addAttribute("title", "Login Page");
-		return "login";
-	}
-
-	@ResponseBody
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public HashMap<String, Object> loginAPI(HttpServletRequest req, HttpServletResponse res) {
- 		// Function variables
-		Span span = GlobalTracer.get().activeSpan();
-		HashMap<String, Object> body = Helpers.decodeBody(req);
-		HashMap<String, Object> output = new HashMap<String, Object>();
-        Logger logger = LogManager.getLogger("vulcan");
-
-		try {
-			// Validate the user input
-			if (!Helpers.validate(body)) {
-				res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-				output.put("message", "There was an issue with your request.");
-				return output;
-			}
-
-            // Generate the authorization request body
-            HashMap<String, Object> auth = new HashMap<String, Object>();
-            auth.put("username", body.get("username"));
-			//auth.put("pwHash", DigestUtils.sha256Hex(body.get("password").toString()));
-			auth.put("pwHash", body.get("password"));
-
-            // Make authentication request to authenticator service
-            HttpResponse<String> response = Helpers.httpPostRequest(new URI("https://authenticator:2884/authenticate"), auth);
-
- 			// Handle response
-			switch (response.statusCode()) {
-   				case HttpServletResponse.SC_OK:
-					res.setStatus(HttpServletResponse.SC_OK);
-					req.getSession().setAttribute("username", body.get("username"));
-					output.put("message", "Successfully logged in.");
-					logger.info(body.get("username") + " logged in");
-					return output;
-
-				case HttpServletResponse.SC_UNAUTHORIZED:
-					res.setStatus(HttpServletResponse.SC_FORBIDDEN);
-					output.put("message", "Your login details are incorrect.");
-					logger.info(body.get("username") + " failed to log in");
-					return output;
-
-				default:
-					throw new Exception("VulcanError: couldn't authorize request");
-			}
-		} catch (Exception e) {
-			res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			output.put("message", "There was an issue with the Server, please try again later.");
-
-			span.setTag(Tags.ERROR, true);
-			span.log(Collections.singletonMap(Fields.ERROR_OBJECT, e));
-
-			return output;
-		}
-	}
-
-	@RequestMapping(value = "/logout", method = RequestMethod.GET)
-	public void logoutAPI(HttpServletRequest req, HttpServletResponse res) {
-		// Function variables
-		Logger logger = LogManager.getLogger("vulcan");
-		HashMap<String, Object> body = Helpers.decodeBody(req);
-
-		// Log out user
-		req.getSession().invalidate();
-		logger.info(body.get("username") + " logged out");
-		res.setStatus(HttpServletResponse.SC_OK);
-	}
-
 	@RequestMapping(value = "/user/{username}", method = RequestMethod.GET)
 	public String userPage(String username, Model model) {
-		model.addAttribute("title", "User");
+		model.addAttribute("title", "User: " + username);
+		model.addAttribute("username", username);
 		return "user";
 	}
 
@@ -117,4 +36,103 @@ public class Users {
 		model.addAttribute("title", "Join Vulcan");
 		return "join";
 	}
+
+	@RequestMapping(value = "/user/create", method = RequestMethod.POST)
+	public HashMap<String, Object> userCreateAPI(HttpServletRequest req, HttpServletResponse res) {
+        // Function variables
+        Span span = GlobalTracer.get().activeSpan();
+        HashMap<String, Object> body = Helpers.decodeBody(req);
+        HashMap<String, Object> output = new HashMap<>();
+        Logger logger = LogManager.getLogger("vulcan");
+
+        // Validate the user input
+        if (!Helpers.validate(body)) {
+            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            output.put("message", "There was an issue with your request.");
+            return output;
+        }
+
+        try {
+            // Build user object
+            HashMap<String, Object> user = new HashMap<>();
+			user.put("username", body.get("username"));
+            user.put("pwhash", body.get("username"));
+
+            // Make user request
+            HttpResponse<String> response = Helpers.httpPostRequest(new URI("https://user-manager:910/create"), user);
+
+            // Handle user response
+            switch (response.statusCode()) {
+                case HttpServletResponse.SC_OK:
+                    res.setStatus(HttpServletResponse.SC_OK);
+                    logger.info("created user " + body.get("username"));
+                    output.put("username", body.get("username"));
+                    return output;
+
+                default:
+                    throw new Exception("VulcanError: unexpected response from user-manager");
+            }
+        } catch (Exception e) {
+            res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            output.put("message", "There was an issue with the Server, please try again later.");
+
+            span.setTag(Tags.ERROR, true);
+            span.log(Collections.singletonMap(Fields.ERROR_OBJECT, e));
+
+            logger.error("vulcan encountered error during user creation: " + e.getMessage(), e);
+            return output;
+        }
+	}
+
+	@RequestMapping(value = "/user/delete", method = RequestMethod.POST)
+	public HashMap<String, Object> userDeleteAPI(HttpServletRequest req, HttpServletResponse res) {
+        // Function variables
+        Span span = GlobalTracer.get().activeSpan();
+        HashMap<String, Object> body = Helpers.decodeBody(req);
+        HashMap<String, Object> output = new HashMap<>();
+        Logger logger = LogManager.getLogger("vulcan");
+
+        // Validate the user input
+        if (!Helpers.validate(body)) {
+            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            output.put("message", "There was an issue with your request.");
+            return output;
+        }
+
+        try {
+            // Prep user request
+            HashMap<String, Object> username = new HashMap<String, Object>();
+            username.put("username", body.get("username"));
+
+            // Make authorization request to authenticator service
+            HttpResponse<String> response = Helpers.httpPostRequest(new URI("https://user-manager:910/delete"), username);
+
+            // Handle response
+            switch (response.statusCode()) {
+                case HttpServletResponse.SC_OK:
+                    res.setStatus(HttpServletResponse.SC_OK);
+                    logger.info("deleted user " + body.get("username"));
+                    output.put("message", "Successfully deleted user.");
+                    return output;
+
+                case HttpServletResponse.SC_NOT_FOUND:
+                    res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                    logger.info("user not found for username " + body.get("username"));
+                    output.put("message", "Couldn't find a user with that username.");
+                    return output;
+
+                default:
+                    throw new Exception("VulcanError: unexpected response from user-manager");
+            }
+        } catch (Exception e) {
+            res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            output.put("message", "There was an issue with the Server, please try again later.");
+
+            span.setTag(Tags.ERROR, true);
+            span.log(Collections.singletonMap(Fields.ERROR_OBJECT, e));
+
+            logger.error("vulcan encountered error during user deletion: " + e.getMessage(), e);
+            return output;
+        }
+    }
 }
