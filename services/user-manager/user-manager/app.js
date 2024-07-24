34c7ea338e6bc30b8595f5f6df9d2abbd70f1c80
MatthewBrazill <matthew.brazill@datadoghq.com>
2024-07-24 10:42:45 +0200

    disabled tracing for net package

--- services/user-manager/user-manager/app.js
+++ services/user-manager/user-manager/app.js
@@ -13,6 +13,7 @@ tracer.init({
     }
 })
 tracer.use("dns", { enabled: false })
+tracer.use("net", { enabled: false })
 tracer.use("redis", { service: "session-store" })
 tracer.use("pg", { dbmPropagationMode: 'full', service: "user-database" })
 tracer.use("mongodb-core", { service: "god-database" })
