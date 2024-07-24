e738af22796e31852df900f5cc57bcc6633a16fc
MatthewBrazill <matthew.brazill@datadoghq.com>
2024-07-24 10:44:10 +0200

    added error log for database connection loss

--- services/user-manager/user-manager/databases.js
+++ services/user-manager/user-manager/databases.js
@@ -2,11 +2,16 @@
 
 // Imports
 const postgresql = require("pg")
+const logger = require("./logger")
 
 const databases = {
     async userDatabase() {
         const pgdb = new postgresql.Client({ connectionString: "postgresql://vulcan:yKCstvg4hrB9pmDP@database-proxy:5432/vulcan_users" })
         pgdb.connect()
+
+        pgdb.on("error", (err) => {
+            logger.error("postgres connection failed with error: ", err)
+        })
 
         return pgdb
     }
