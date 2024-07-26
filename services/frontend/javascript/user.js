cf5cecec4e0a09ed567d16679747e9129b3672f4
MatthewBrazill <matthew.brazill@datadoghq.com>
2024-07-26 17:07:33 +0200

    added user page to frontend

--- services/frontend/javascript/user.js
+++ services/frontend/javascript/user.js
@@ -1,1 +1,50 @@
-$(document).ready(() => { })
+$(document).ready(() => {
+    $.ajax({
+        url: "/users/all",
+        method: "GET",
+        beforeSend: () => {
+            // Update content
+            while ($("#user-list").children().length > 1) {
+                $("#user-list").children().last().remove()
+            }
+
+            // Make style adjustments
+            $("#user-list").attr("class", "")
+            $("#user-list-loader").attr("class", "ui active text loader")
+            $("#user-list-error").attr('class', 'ui hidden error message')
+        },
+        success: (res) => {
+            // Update content
+            if (res.users.length > 0) {
+                for (var user of res.users) {
+                    item = `
+                    <a href="/user/${user.username}">
+                        <div class="user-item">
+                            <h4 class="user-header">${user.username}</h4>
+                        </div>
+                    </a>`
+                    $("#user-list").append(item)
+                }
+            } else {
+                $("#user-list").append(`
+                <div class="user-item">
+                    <h2 class="user-header">No Users Found</h2>
+                </div>`)
+            }
+
+            // Make style adjustments
+            $("#user-list").attr("class", "")
+            $("#user-list-loader").attr("class", "ui hidden text loader")
+            $("#user-list-error").attr('class', 'ui hidden error message')
+        },
+        error: (res) => {
+            // Update content
+            $("#user-list-error-message").text(res.message)
+
+            // Make style adjustments
+            $("#user-list").attr("class", "")
+            $("#user-list-loader").attr("class", "ui hidden text loader")
+            $("#user-list-error").attr('class', 'ui error message')
+        }
+    }).then()
+})
