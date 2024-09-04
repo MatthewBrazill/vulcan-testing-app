3aa6ca0a70dafd6bc693f97cbb6ca8f2c1efafaf
MatthewBrazill <matthew.brazill@datadoghq.com>
2024-09-04 15:48:48 +0200

    added datadog orchestrion for automatic go instrumentation

---
+++ services/god-manager/tools.go
@@ -0,0 +1,14 @@
+//go:build tools
+
+/*
+
+This file ensures that the tools needed are still included after
+running `go mod tidy` and that the imports continue to work.
+
+It will always show an error, but this can be safely ignored
+
+*/
+
+package main
+
+import _ "github.com/datadog/orchestrion"
