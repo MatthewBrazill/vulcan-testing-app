969e4dadf935f15a3112398a4a545a34f0ae6dc7
MatthewBrazill <matthew.brazill@datadoghq.com>
2024-09-20 14:04:52 +0200

    changed java to use the default (hopefully) cacert store

--- build-scripts/startup.sh
+++ build-scripts/startup.sh
@@ -22,15 +22,13 @@ export DD_GIT_REPOSITORY_URL=$(git config --get remote.origin.url)
 echo "starting service $DD_SERVICE..."
 case $DD_SERVICE in
     "vulcan")
-        touch /cacerts/keystore.jks
-        chmod 644 /cacerts/keystore.jks
-        keytool -import -noprompt -alias user-manager-cert -keystore /cacerts/keystore.jks -file /vulcan/services/user-manager/certificate/cert.pem -storepass changeit
-        keytool -import -noprompt -alias god-manager-cert -keystore /cacerts/keystore.jks -file /vulcan/services/god-manager/certificate/cert.pem -storepass changeit
-        keytool -import -noprompt -alias authenticator-cert -keystore /cacerts/keystore.jks -file /vulcan/services/authenticator/certificate/cert.pem -storepass changeit
+        keytool -import -noprompt -alias user-manager-cert -keystore $JAVA_HOME/jre/lib/security/cacerts -file /vulcan/services/user-manager/certificate/cert.pem -storepass changeit
+        keytool -import -noprompt -alias god-manager-cert -keystore $JAVA_HOME/jre/lib/security/cacerts -file /vulcan/services/god-manager/certificate/cert.pem -storepass changeit
+        keytool -import -noprompt -alias authenticator-cert -keystore $JAVA_HOME/jre/lib/security/cacerts -file /vulcan/services/authenticator/certificate/cert.pem -storepass changeit
         wget -nc -nv -O /dd-java-agent.jar https://dtdg.co/latest-java-tracer
         mvn install
         java -javaagent:/dd-java-agent.jar \
-            -Djavax.net.ssl.trustStore=/cacerts/keystore.jks \
+            -Djavax.net.ssl.trustStore=$JAVA_HOME/jre/lib/security/cacerts \
             -Djavax.net.ssl.trustStorePassword=changeit \
             -Dlog4j2.configurationFile=/vulcan/services/vulcan/src/log4j2.xml \
             -Dvulcan.session.key=$VLCN_SESSION_KEY \
