267d6a79bb8996b67d572ec057ef94107f6b8c30
MatthewBrazill <matthew.brazill@datadoghq.com>
2024-09-20 15:14:46 +0200

    updated java config to use `--cacerts` instead of cacert path

--- build-scripts/startup.sh
+++ build-scripts/startup.sh
@@ -22,14 +22,12 @@ export DD_GIT_REPOSITORY_URL=$(git config --get remote.origin.url)
 echo "starting service $DD_SERVICE..."
 case $DD_SERVICE in
     "vulcan")
-        keytool -import -noprompt -alias user-manager-cert -keystore $JAVA_HOME/lib/security/cacerts -file /vulcan/services/user-manager/certificate/cert.pem -storepass changeit
-        keytool -import -noprompt -alias god-manager-cert -keystore $JAVA_HOME/lib/security/cacerts -file /vulcan/services/god-manager/certificate/cert.pem -storepass changeit
-        keytool -import -noprompt -alias authenticator-cert -keystore $JAVA_HOME/lib/security/cacerts -file /vulcan/services/authenticator/certificate/cert.pem -storepass changeit
+        keytool -import -noprompt -alias user-manager-cert -cacerts -file /vulcan/services/user-manager/certificate/cert.pem -storepass changeit
+        keytool -import -noprompt -alias god-manager-cert -cacerts -file /vulcan/services/god-manager/certificate/cert.pem -storepass changeit
+        keytool -import -noprompt -alias authenticator-cert -cacerts -file /vulcan/services/authenticator/certificate/cert.pem -storepass changeit
         wget -nc -nv -O /dd-java-agent.jar https://dtdg.co/latest-java-tracer
         mvn install
         java -javaagent:/dd-java-agent.jar \
-            -Djavax.net.ssl.trustStore=$JAVA_HOME/lib/security/cacerts \
-            -Djavax.net.ssl.trustStorePassword=changeit \
             -Dlog4j2.configurationFile=/vulcan/services/vulcan/src/log4j2.xml \
             -Dvulcan.session.key=$VLCN_SESSION_KEY \
             -Ddd.trace.agent.url=$DD_TRACE_AGENT_URL \
