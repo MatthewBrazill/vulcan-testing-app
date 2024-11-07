98d5c25d07706beacf74211ba7c5d362baa6629c
MatthewBrazill <matthew.brazill@datadoghq.com>
2024-11-07 11:06:48 +0100

    fixed authentication and user issues, plus added default values to setup script

--- services/databases/god-database/setup.js
+++ services/databases/god-database/setup.js
@@ -4,17 +4,29 @@ db.createUser({
     "pwd": "5aae8c35f7e16245",
     "roles": [
         { role: "read", db: "admin" },
-        { role: "read", db: "vulcan" },
         { role: "read", db: "local" },
+        { role: "readAnyDatabase", db: "admin" },
         { role: "clusterMonitor", db: "admin" }
     ]
 })
-
-use("vulcan")
 db.createUser({
     "user": "vulcan",
     "pwd": "96758wg54tbravp7",
     "roles": [
         { role: "readWrite", db: "vulcan" }
     ]
+})
+
+use("vulcan")
+db.gods.insertOne({
+    "godId": "H62nd",
+    "pantheon": "Roman",
+    "name": "Justitia",
+    "domain": "Justice and Morals",
+})
+db.gods.insertOne({
+    "godId": "Zu72H",
+    "pantheon": "Greek",
+    "name": "Zeus",
+    "domain": "Sky and Lightning",
 })
--- services/databases/notes-database/setup.js
+++ services/databases/notes-database/setup.js
@@ -4,17 +4,47 @@ db.createUser({
     "pwd": "5aae8c35f7e16245",
     "roles": [
         { role: "read", db: "admin" },
-        { role: "read", db: "notes" },
         { role: "read", db: "local" },
+        { role: "readAnyDatabase", db: "admin" },
         { role: "clusterMonitor", db: "admin" }
     ]
 })
-
-use("notes")
 db.createUser({
     "user": "notes",
     "pwd": "96758wg54tbravp7",
     "roles": [
         { role: "readWrite", db: "notes" }
     ]
+})
+
+use("notes")
+db.notes.updateOne({
+    'username': 'matthew'
+}, {
+    $addToSet: {
+        'notes': 'This user is the owner of the application.'
+    }
+}, {
+    'upsert': true
+})
+db.notes.updateOne({
+    'username': 'synthetics'
+
+}, {
+    $addToSet: {
+        'notes': 'This is a synthetic account.'
+
+    }
+}, {
+    'upsert': true
+})
+db.notes.updateOne({
+    'username': 'synthetics'
+
+}, {
+    $addToSet: {
+        'notes': 'It is used only for automated testing.'
+    }
+}, {
+    'upsert': true
 })
