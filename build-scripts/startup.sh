4a039870c05e09d7ac90a2c1a56a9240b417fd45
Matthew Brazill <matthew.brazill@datadoghq.com>
2025-11-13 16:48:50 +0100

    fix: remove error logging redirection from startup script for cleaner output

--- build-scripts/startup.sh
+++ build-scripts/startup.sh
@@ -106,5 +106,5 @@
             ;;
     esac
     echo "looks like something went wrong" >&2
-} > >(sed "s/^/$(date +%s%N | cut -b1-13) info: /") 2> >(sed "s/^/$(date +%s%N | cut -b1-13) error: /")
+} > >(sed "s/^/$(date +%s%N | cut -b1-13) info: /")
 exit 1
