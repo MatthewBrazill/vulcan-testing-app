f8f5398f82a01d164c6f061a63501b63c5940dcb
Matthew Brazill <matthew.brazill@datadoghq.com>
2026-03-11 17:01:21 +0100

    fix: change database functions to use gin context

--- services/god-manager/god-manager/databases.go
+++ services/god-manager/god-manager/databases.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"context"
-
+	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 //dd:span resource_name:Databases.GodDatabase operation:god-manager.database
-func GodDatabase(ctx context.Context) (*mongo.Client, error) {
+func GodDatabase(ctx *gin.Context) (*mongo.Client, error) {
 	var client *mongo.Client
 
 	// Connect to database
 	Log(ctx).Debug("connecting to vulcan-gods")
 	options := options.Client().ApplyURI("mongodb://vulcan-gods:96758wg54tbravp7@mac-mongo.database:27017/?connect=direct&authSource=admin")
-	client, err := mongo.Connect(ctx, options)
+	client, err := mongo.Connect(ctx.Request.Context(), options)
 	if err != nil {
+		Log(ctx).WithError(err).Error(ctx.Error(err).Error())
 		return nil, err
 	}
 
@@ -24,14 +24,15 @@ func GodDatabase(ctx context.Context) (*mongo.Client, error) {
 }
 
 //dd:span resource_name:Databases.NoteDatabase operation:god-manager.database
-func NoteDatabase(ctx context.Context) (*mongo.Client, error) {
+func NoteDatabase(ctx *gin.Context) (*mongo.Client, error) {
 	var client *mongo.Client
 
 	// Connect to database
 	Log(ctx).Debug("connecting to note-database")
 	options := options.Client().ApplyURI("mongodb://vulcan-notes:96758wg54tbravp7@mac-mongo.database:27017/?connect=direct&authSource=admin")
-	client, err := mongo.Connect(ctx, options)
+	client, err := mongo.Connect(ctx.Request.Context(), options)
 	if err != nil {
+		Log(ctx).WithError(err).Error(ctx.Error(err).Error())
 		return nil, err
 	}
 
